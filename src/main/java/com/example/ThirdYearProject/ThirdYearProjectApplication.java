1730b3aa530735ecff484b565a1ba1531a1eb700
luizacomanescu <comanescu.luiza@gmail.com>
2020-11-09 17:51:45 +0000

    Add project to git

---
+++ src/main/java/com/example/ThirdYearProject/ThirdYearProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.ThirdYearProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThirdYearProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ThirdYearProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/ThirdYearProject/ThirdYearProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ThirdYearProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThirdYearProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
